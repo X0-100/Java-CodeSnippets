f3cc4fbadabbdba90eda0b68c542942d29db1a1e
Saurabh Sanyal <saurabh.sanyal@newgensoft.com>
2022-12-24 02:13:53 -0800

    02:13 24 Dec 2022

---
+++ NullPointerException.java
@@ -0,0 +1,14 @@
+/* Throwing null pointer exception  */
+
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String str = null;
+        
+       int length = str.length();
+        
+    }
+}
+
+/* Catch : Simplest way to call NPE */
